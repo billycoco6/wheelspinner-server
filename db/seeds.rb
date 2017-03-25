3ff703fd87f09b0afcdae613bbfa3b88cb5645de
Billy P. Witt <billy.coco6@gmail.com>
2017-03-25 15:54:31 +0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(path: "../../static/1.jpg", title: "Fashion")
+Category.create!(path: "../../static/2.jpg", title: "Wearables")
+Category.create!(path: "../../static/3.jpg", title: "Games")
+Category.create!(path: "../../static/4.jpg", title: "Sports")
+Category.create!(path: "../../static/5.jpg", title: "Collectibles")
+Category.create!(path: "../../static/6.jpg", title: "Home and Garden")
+Category.create!(path: "../../static/7.jpg", title: "Books")
+Category.create!(path: "../../static/8.jpg", title: "Beauty")
